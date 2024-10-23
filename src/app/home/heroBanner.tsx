3ee96edd06d82f1d5fc02f9212b3cee4903a004a
Suveer Grover <119030604+GoGeek91@users.noreply.github.com>
2024-10-23 18:34:02 +0530

    fix:design

--- src/app/home/heroBanner.tsx
+++ src/app/home/heroBanner.tsx
@@ -1,22 +1,26 @@
 import React from "react";
 import { BackgroundBeamsWithCollision } from "../components/ui/heroAnimation";
 import Image from "next/image";
+import { FlipWords } from "../components/ui/flipWords";
 
 export const HeroBanner = () => {
+  const words = ["better", "cute", "beautiful", "modern"];
   return (
     <>
       <BackgroundBeamsWithCollision>
-        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8 px-4">
-          <div className="text-center space-y-4">
-            <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500 text-6xl font-bold">
-              ORICMETA TECHNOLOGY
-            </h1>
-            <h2 className="text-white text-4xl">India-Based Digital Agency</h2>
-            <h3 className="text-white text-xl font-light">
-              Oricmeta is a Website Design & Development Firm specialized with Digital Solution. our young and
-              experienced team incorporates the perfect mix of insightful designs, calculated tactics and
-              supreme creativity along with state of art technology to build result driven relations.
-            </h3>
+        <div className="mx-auto max-w-7xl">
+          <div className="grid grid-cols-2">
+            <div className="text-center space-y-4">
+              <h1 className="">ORICMETA TECHNOLOGY</h1>
+              <h2 className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500 text-5xl font-bold text-left">
+                Help You To <FlipWords words={words} />{" "}
+              </h2>
+              <h3 className="text-white text-lg font-light text-justify">
+                Oricmeta is a Website Design & Development Firm specialized with Digital Solution. our young
+                and experienced team incorporates the perfect mix of insightful designs, calculated tactics
+                and supreme creativity along with state of art technology to build result driven relations.
+              </h3>
+            </div>
           </div>
         </div>
       </BackgroundBeamsWithCollision>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import { HeroBanner } from "./home/heroBanner";
+import { About } from "./home/about";
 
 export default function Home() {
   return (
     <>
       <HeroBanner />
+      <About />
     </>
   );
 }
